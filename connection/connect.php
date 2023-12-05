b55d124d4f7dc63ad1255d1f5e04fd74df05f19a
sainath2045 <151537734+sainath2045@users.noreply.github.com>
2023-12-06 03:06:48 +0530

    Update connect.php

--- connection/connect.php
+++ connection/connect.php
@@ -1,9 +1,9 @@
 <?php
 
 //main connection file for both admin & front end
-$servername = "localhost"; //server
-$username = "root"; //username
-$password = ""; //password
+$servername = "jyosql.mysql.database.azure.com"; //server
+$username = "jyo"; //username
+$password = "qwerty@12345"; //password
 $dbname = "online_rest";  //database
 
 // Create connection
